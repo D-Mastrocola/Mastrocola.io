a14bbeecf7cd2afbe9685b8aab79a38b0507a402
D-Mastrocola <mastrodomcola@gmail.com>
2021-09-29 12:52:03 -0400

    use mouse to draw to the canvas

--- src/Components/Artboard/Artboard.jsx
+++ src/Components/Artboard/Artboard.jsx
@@ -9,18 +9,28 @@ class Artboard extends Component {
     this.myRef = React.createRef();
   }
   initalizeArtboard(p) {
+    let width = 400;
+    let height = 400;
     let x = 100;
     let y = 100;
+    let gridSize = width/16;
 
     p.setup = () => {
       p.createCanvas(400, 400);
+      p.background(0)
     };
 
     p.draw = () => {
-      p.background(0);
-      p.fill(255);
-      p.rect(x, y, 50, 50);
     };
+    p.mousePressed = () => {
+      console.log(p.mouseX)
+      if(p.mouseX >= 0 && p.mouseX < width && p.mouseY >= 0 && p.mouseY < height) {
+        let gridNode = [Math.floor(p.mouseX/gridSize),Math.floor(p.mouseY/gridSize)]
+        p.fill(255)
+        p.noStroke();
+        p.rect(gridNode[0] * gridSize, gridNode[1] * gridSize, gridSize, gridSize)
+      }
+    }
   }
   componentDidMount() {
     this.myP5 = new p5(this.initalizeArtboard, this.myRef.current);
