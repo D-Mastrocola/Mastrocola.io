276bae5f1ed0e543eb5f2720e108c318f56ca07d
D-Mastrocola <mastrodomcola@gmail.com>
2021-09-29 12:40:40 -0400

    integrated p5.js with React

--- src/Components/Artboard/Artboard.jsx
+++ src/Components/Artboard/Artboard.jsx
@@ -1,23 +1,38 @@
 import { Component } from "react";
+import React from "react";
+import p5 from "p5";
+
 
 class Artboard extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.myRef = React.createRef();
   }
-  initalizeArtboard() {
-      console.log('load artboard')
+  initalizeArtboard(p) {
+    let x = 100;
+    let y = 100;
+
+    p.setup = () => {
+      p.createCanvas(400, 400);
+    };
+
+    p.draw = () => {
+      p.background(0);
+      p.fill(255);
+      p.rect(x, y, 50, 50);
+    };
   }
   componentDidMount() {
-    this.initalizeArtboard();
+    this.myP5 = new p5(this.initalizeArtboard, this.myRef.current);
   }
   render() {
-    return(
+    return (
       <main className="col-12 d-flex justify-content-center flex-column align-items-center">
         <h2>Pixel Editor</h2>
-        <canvas id='artboard-canvas'></canvas>
-        <button className='btn btn-primary'>Submit</button>
+        <div ref={this.myRef}></div>
+        <button className="btn btn-primary">Submit</button>
       </main>
-    )
+    );
   }
 }
 export default Artboard;
